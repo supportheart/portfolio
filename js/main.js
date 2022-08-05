b34f77ac031a27a7c9416ec7ee35d457ce9ce2ec
supportheart <seongwonheart@gmail.com>
2022-08-05 18:00:06 +0900

    portfolio_08_05_03

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 window.onload = function () {
+  const i = this.document.querySelectorAll('.intro_text span');
 
   // swiper
   const swiper = new Swiper('.swiper', {
@@ -16,13 +17,35 @@ window.onload = function () {
       prevEl: '.swiper-button-prev',
     },
   });
+  hello(true);
+  window.addEventListener('scroll', throttle(function () {
+    if (document.documentElement.scrollTop < 20) {
+      hello(true);
+    } else {
+      hello(false);
+    }
+  }, 200));
 
-  window.addEventListener('scroll', function () {
-    if (document.documentElement.scrollTop < 1) {
-      const i = this.document.querySelectorAll('.intro_text span');
+  function hello(bool) {
+    if (bool) {
       i.forEach(function (item) {
-        item.classList.toggle("hello");
+        item.classList.add("hello");
+      });
+    } else {
+      i.forEach(function (item) {
+        item.classList.remove("hello");
       });
     }
-  });
+  }
 }
+
+const throttle = (callback, delay) => {
+  let timer;
+  return (event) => {
+    if (timer) return;
+    timer = setTimeout(() => {
+      callback(event);
+      timer = null;
+    }, delay);
+  }
+};
